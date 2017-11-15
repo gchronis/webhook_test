cace58e8c5dcd1f8f6016ad5b72b78efa232d00c
Gabriella Chronis <gchronis@polysync.io>
2017-11-15 14:01:29 -0800

    Send response back to client

--- webhook_webapp/api/views.py
+++ webhook_webapp/api/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -11,4 +12,4 @@ from django.views.decorators.csrf import csrf_exempt
 def post(request):
     print("I got some JSON data:")
     print(request)
-
+    return HttpResponse('received payload')
