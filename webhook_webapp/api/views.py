10f191b33af38cda32dde828b0112c83788bd65b
Gabriella Chronis <gchronis@polysync.io>
2017-11-15 13:56:34 -0800

    Don't require CSRF token because the request is coming from GH

--- webhook_webapp/api/views.py
+++ webhook_webapp/api/views.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def post(request):
     print("I got some JSON data:")
     print(request)
